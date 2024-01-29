ec2e589f4443fe53c7487ca2a128dd04dcb441cf
zhuangzhi213 <1716571003@qq.com>
2024-01-29 17:06:39 +0800

    Site updated: 2024-01-29 17:06:24

---
+++ js/zz-random-images.js
@@ -0,0 +1,28 @@
+// 定义一个包含图片URL的数组
+var imageUrls = [
+  "/img/card-announcement-light01.svg",
+  "/img/card-announcement-light02.svg",
+  "/img/card-announcement-light03.svg",
+  "/img/card-announcement-light04.svg",
+];
+
+function getRandomImage() {
+  var index = Math.floor(Math.random() * imageUrls.length);
+  return imageUrls[index];
+}
+
+// 在页面加载完成后替换图片
+window.onload = function() {
+  var imgElement = document.getElementById('randomImage');
+  if (imgElement) {
+    imgElement.src = getRandomImage();
+  }
+};
+// 获取或创建一个HTML元素，并设置其背景图片为随机图片
+var element = document.getElementById('card-announcement');
+if (element) {
+  var randomImageUrl = getRandomImage();
+  element.style.backgroundImage = 'url("' + randomImageUrl + '")';
+}
+
+document.documentElement.style.setProperty('--zz-card-announcementlight', `url("${getRandomImage()}")`);
